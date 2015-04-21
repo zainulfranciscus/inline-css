e5136e071495a0366f83fa7e347ae33464a8ccb7
Zainul Franciscus <zainul.franciscus@gmail.com>
2015-04-22 09:18:45 +1000

    test inline css with multiple html files

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,24 @@
  var gulp = require('gulp'),
-     inlineCss = require('gulp-inline-css'); 
+     inlineCss = require('gulp-inline-css');
 
  // file locations
 var
 	source = 'source/',
-	dest = 'build/';
-
-
+	dest = 'build/',
+	html = source + "/**/*.html";
 
 gulp.task('inline-css',function(){
-	return gulp.src(source + "/**/*.html").
-	       pipe(inlineCss({
+	return gulp.src(html)
+	       .pipe(inlineCss({
                 applyStyleTags: true,
                 applyLinkTags: true,
                 removeStyleTags: true,
                 removeLinkTags: true
-        }))
+        		}))
 		   .pipe(gulp.dest(dest));
 });
 // default task
 gulp.task('default', ['inline-css'],function() {
+	gulp.watch(html,['inline-css']);
 
 });
