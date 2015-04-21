bf3777db47e2ff82e484fe8ec0590d587b6ededa
Zainul Franciscus <zainul.franciscus@gmail.com>
2015-04-22 09:54:20 +1000

    install browsersync to load HTML files in localhost 3000

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,47 @@
  var gulp = require('gulp'),
-     inlineCss = require('gulp-inline-css');
+     inlineCss = require('gulp-inline-css'),
+     browsersync = require('browser-sync');
 
  // file locations
 var
 	source = 'source/',
 	dest = 'build/',
-	html = source + "/**/*.html";
+	html = source + "/**/*.html",
+	css = source + "/**/*.css";
 
-gulp.task('inline-css',function(){
-	return gulp.src(html)
-	       .pipe(inlineCss({
+var htmlAndCss = [html,css];
+
+//this task will create inline css
+var inlineCssOpts = {
                 applyStyleTags: true,
                 applyLinkTags: true,
                 removeStyleTags: true,
                 removeLinkTags: true
-        		}))
-		   .pipe(gulp.dest(dest));
+}
+
+gulp.task('inline-css',function(){
+	return gulp.src(htmlAndCss)
+	       .pipe(inlineCss(inlineCssOpts))
+		   .pipe(gulp.dest(dest))
+		   .pipe(browsersync.reload({ stream: true }));
 });
+
+//this task will reload a browser using browser-sync
+var syncOpts = {
+	server: {
+		baseDir: dest,
+		index: 'index.html'
+	},
+	open: false,
+	notify: true
+};
+
+gulp.task('browsersync', function() {
+	browsersync(syncOpts);
+});
+
 // default task
-gulp.task('default', ['inline-css'],function() {
-	gulp.watch(html,['inline-css']);
+gulp.task('default', ['inline-css','browsersync'],function() {
+	gulp.watch(htmlAndCss,['inline-css',browsersync.reload]);
 
 });
